57c80cb3a9dfea20868e1823e8d6f7fea2191c52
Alisson Pereira Santana <alissonpereira1993@gmail.com>
2021-08-26 22:38:43 -0300

    pageview test

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -1,6 +1,20 @@
-// Preencha este arquivo com qualquer código que você necessite para realizar a
-// coleta, desde a biblioteca analytics.js, gtag.js ou o snippet do Google Tag 
-// Manager. No último caso, não é necessário implementar a tag <noscript>.
-// O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
-// para fazer a sua coleta.
-// Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+/*
+Variables
+*/
+
+const userID = 'UA-206106142-1'
+
+/*
+Start
+*/
+
+ga('create', userID, 'auto')
+
+/*
+PageView
+*/
+
+ga('send', {
+    hitType: 'pageview',
+    page: location.pathname
+})
